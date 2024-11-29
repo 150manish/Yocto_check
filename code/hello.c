4c4078bab200d321be7a83ec188ef95daf5a470c
mangupt <mangupt@qti.qualcomm.com>
2024-11-29 15:54:04 +0530

    Hello world code.
    Change-Id: Icaf2d718b14111766174efa66a2d9bfa078c3eca

---
+++ code/hello.c
@@ -0,0 +1,17 @@
+/*
+This is Hello World program for Yocto check.
+
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv) {
+
+	printf("\n* Hello World *\n");
+	return 0;
+}
+
+
+
