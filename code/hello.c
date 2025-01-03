3b0b4e875c250dd0a36fdd3a3d124d907e9cc567
mangupt <mangupt@qti.qualcomm.com>
2025-01-03 13:45:05 +0530

    Add .h
    Change-Id: I1c594bdbd1a5dae1177ecc478d781bcbe213b3dd

--- code/hello.c
+++ code/hello.c
@@ -3,13 +3,11 @@ This is Hello World program for Yocto check.
 
 */
 #include <stdio.h>
-#include <stdint.h>
-#include <unistd.h>
-
+#include "hello.h"
 
 int main(int argc, char **argv) {
 
-	printf("\n* Hello World *\n");
+	printf("\n* Hello World %d *\n", SAMPLE_MACRO-256);
 	return 0;
 }
 
---
+++ code/hello.h
@@ -0,0 +1,9 @@
+/*
+This is Hello World program for Yocto check.
+
+*/
+
+#include <stdint.h>
+#include <unistd.h>
+
+#define SAMPLE_MACRO 256
